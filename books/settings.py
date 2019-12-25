c8e5e7b61e36b3aed35e85d71347f9b6d4c66c19
zhalelov <e.zhalelov@gmail.com>
2019-12-25 13:35:10 +0600

    delete psy

--- books/settings.py
+++ books/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '6@nkkpkr24gw)jn0x=qtw$7_98b--v
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = False
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ['*']
 
