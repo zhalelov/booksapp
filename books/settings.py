5f34f1cf68fc03a38c010919d76832e979da7979
zhalelov <e.zhalelov@gmail.com>
2019-12-26 17:34:12 +0600

    change req

--- books/settings.py
+++ books/settings.py
@@ -131,6 +131,7 @@ STATICFILES_DIRS = [
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
 import dj_database_url
-db_from_env = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
